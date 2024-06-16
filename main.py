d6a14740206b72459bde6d9a73ec41efed5effb4
PangYuanbo <yuanbopang@outlook.com>
2024-06-15 18:57:56 -0700

    initial

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-# Load model directly
-from transformers import AutoModelForCausalLM
-model = AutoModelForCausalLM.from_pretrained("liuhaotian/llava-v1.6-mistral-7b")
+from PIL import Image
+import requests
+from transformers import AutoProcessor, LlavaForConditionalGeneration
+model = LlavaForConditionalGeneration.from_pretrained("llava-hf/llava-1.5-7b-hf")
+processor = AutoProcessor.from_pretrained("llava-hf/llava-1.5-7b-hf")
+prompt = "USER: <image>\nWhat's the content of the image? ASSISTANT:"
+url = "https://www.ilankelman.org/stopsigns/australia.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+inputs = processor(prompt, image, return_tensors="pt")
+generate_ids = model.generate(**inputs, max_new_tokens=15)
+processor.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
